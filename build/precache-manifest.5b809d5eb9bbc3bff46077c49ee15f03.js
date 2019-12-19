ae28fec8d3410a21240b36928dc2f3c32445d2c2
xingji.shuang <xingji.shuang@yitu-inc.com>
2019-12-19 17:02:41 +0800

    add

---
+++ build/precache-manifest.5b809d5eb9bbc3bff46077c49ee15f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df5e954a7c2e5a1178c3583ce32c4ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b535680829526d088996",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a40f01118a4e8c5a2a70",
+    "url": "/static/js/2.18e38093.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.18e38093.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b535680829526d088996",
+    "url": "/static/js/main.3bd80572.chunk.js"
+  },
+  {
+    "revision": "49ab877363a1932de0c1",
+    "url": "/static/js/runtime-main.20239be7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
